f0cfa8eae7eba5576a431d1569ca3300e8c81bbb
Pedro Brasil <phblbrasil@gmail.com>
2024-03-03 20:18:24 -0300

    subindo a parte que começamos a avaliar por grupo

--- Aula_29_02_2024_ANOVA/aula_29_09_2024.py
+++ Aula_29_02_2024_ANOVA/aula_29_09_2024.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from tabulate import tabulate
 
 # %%
 # carregando o banco de dados das notas
@@ -23,7 +24,14 @@ range_preco_chocolate = {
 # %%
 chocolates['range_preco'] = chocolates['faixa_preco'].map(range_preco_chocolate)
 chocolates = chocolates[['faixa_preco', 'range_preco', 'notas']]
-# %%
+chocolates = chocolates.rename(
+    columns={
+        'faixa_preco': 'Faixa de Preço',
+        'range_preco': 'Intervalo do Valor do Chocolate',
+        'notas': 'Notas Recebidas'
+    }
+)
+
 print(chocolates)
 # %%
 # Aqui temos um experimento onde vários indivíduos independentes deram notas para alguns chocolates.
@@ -49,35 +57,61 @@ print(descritivo)
 # %%
 # quartis das notas
 sns.boxplot(data=chocolates, 
-            x="faixa_preco", 
-            y="notas",
+            x='Faixa de Preço', 
+            y='Notas Recebidas',
             color='.8',
             #linecolor="#000",
             linewidth=.75)
-
+plt.title('Comportamento das notas por Faixa de Preço')
+plt.show()
 # %%
 # distribuição das observações no histograma
 sns.histplot(data=chocolates, 
-             x='notas', 
-             bins= 30,
+             x='Notas Recebidas', 
+             bins= 20,
              kde=True)
-
+plt.title('Distribuição das Notas')
+plt.show()
 # %%
 # b.pense em um gráfico interessante para verificar se existe alguma relação
 # entre as notas dadas e o preço do chocolate
-
-sns.scatterplot(data = chocolates, x = 'notas', y = 'faixa_preco', hue = 'range_preco', )
-
+sns.scatterplot(data = chocolates, 
+                x = 'Faixa de Preço', 
+                y = 'Notas Recebidas', 
+                hue = 'Intervalo do Valor do Chocolate')
+plt.title('Relação Faixa de Preço x Notas')
+plt.show()
 # %%
-# c.o que pode ser feito para detectar se existe alguma interação?
+# c. O que pode ser feito para detectar se existe alguma interação?
 
 # %%
 # d.Faça a anova e verifique se as médias das notas, por faixa de preço são iguais.
-
+# começando temos que fazer o passo a passo para podermos fazer a ANOVA, são eles:
+# 1)  
+# 2)
+# 3)
+# 4)
 # %%
 # e.Crie as equações para chegar no chute sofisticada (ychapeu). 
+# primeiramente vamos definir as médias por grupos:
+medias_grupos = chocolates.groupby('Faixa de Preço')['Notas Recebidas'].mean().round(2)
+print(medias_grupos)
+# %%
+media_faixa_preco = {
+    'g1':1.0,
+    'g2':1.0,
+    'g3':3.0,
+    'g4':7.0,
+    'g5':9.0,
+    'g6':9.0
+}
 
 # %%
+# adicionar a media na tabela chocolates por faixa de frupo
+chocolates_medias = chocolates
+# %%
+chocolates_medias['media_grupo'] = chocolates_medias['faixa_preco'].map(media_faixa_preco)
+
 # i.Quantas equações podem ser criadas?
 
 # %%
